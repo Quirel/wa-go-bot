ab870db5b0b8beec6f276784f1c867b587cf546e
zhukov <quirel13@gmail.com>
2021-01-20 19:07:59 +1000

    Reply to messages
    - load .env
    - load schedule.json

--- main.go
+++ main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"fmt"
 	"github.com/Rhymen/go-whatsapp"
+	"github.com/joho/godotenv"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 )
+
+// init - loads .env
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
 
 func main() {
 	//create new WhatsApp connection
@@ -17,8 +25,25 @@ func main() {
 		log.Fatalf("error creating connection: %v\n", err)
 	}
 
+	//get message for current day from schedule
+	msgText, err := getMessageFromSchedule()
+	if err != nil {
+		log.Fatalf("schedule error: %v", err)
+	}
+	if msgText == "" {
+		session, err := wac.Disconnect()
+		if err != nil {
+			log.Fatalf("error disconnecting: %v\n", err)
+		}
+		if err := writeSession(session); err != nil {
+			log.Fatalf("error saving session: %v", err)
+		}
+		fmt.Println("Day is empty. Terminating")
+		// todo: soft terminate without error
+	}
+
 	//add custom handlers
-	wac.AddHandler(&waHandler{wac, uint64(time.Now().Unix())})
+	wac.AddHandler(&waHandler{wac, uint64(time.Now().Unix()), msgText})
 
 	//login or restore
 	err = login(wac)
--- messaging.go
+++ messaging.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/Rhymen/go-whatsapp"
+	"io/ioutil"
 	"log"
 	"os"
 	"strings"
@@ -12,6 +14,7 @@ import (
 type waHandler struct {
 	c         *whatsapp.Conn
 	startTime uint64
+	msg       string
 }
 
 //HandleError needs to be implemented to be a valid WhatsApp handler
@@ -38,22 +41,58 @@ HandleTextMessage - receives messages
 Reply by condition
 */
 func (h *waHandler) HandleTextMessage(message whatsapp.TextMessage) {
-	if message.Info.FromMe || message.Info.Timestamp < h.startTime || !strings.Contains(strings.ToLower(message.Text), "@echo") {
+	chatId, _ := os.LookupEnv("CHAT_ID")
+	search, _ := os.LookupEnv("SEARCH")
+
+	if message.Info.Timestamp >= h.startTime {
+		fmt.Printf("senderJid: %v, id: %v, remoteJid: %v, \nmessage:\t%v\n", message.Info.SenderJid, message.Info.Id, message.Info.RemoteJid,
+			message.Text)
+	}
+
+	// TODO: add sender check
+	if message.Info.Timestamp < h.startTime || !strings.Contains(strings.ToLower(message.Text), search) || message.Info.RemoteJid != chatId {
 		return
 	}
-	fmt.Printf("%v %v %v %v\n\t%v\n", message.Info.Timestamp, message.Info.Id, message.Info.RemoteJid,
-		message.ContextInfo.QuotedMessageID, message.Text)
 
 	msg := whatsapp.TextMessage{
 		Info: whatsapp.MessageInfo{
 			RemoteJid: message.Info.RemoteJid,
 		},
-		Text: "Echo!",
+		Text: h.msg,
 	}
 
 	if _, err := h.c.Send(msg); err != nil {
 		fmt.Fprintf(os.Stderr, "error sending message: %v\n", err)
 	}
 
-	fmt.Printf("echoed message to user %v\n", message.Info.RemoteJid)
+	fmt.Printf("message sent to user %v\n", message.Info.RemoteJid)
+}
+
+/**
+getMessageFromSchedule - reads current schedule from file
+return text message fot today to send
+*/
+func getMessageFromSchedule() (string, error) {
+	msg := ""
+	byteValue, err := ioutil.ReadFile("./schedule.json")
+	if err != nil {
+		log.Fatalf("schedule error: %v", err)
+	}
+
+	type Schedule []struct {
+		Day     int    `json:"day"`
+		Message string `json:"message"`
+	}
+
+	var schedule Schedule
+	json.Unmarshal(byteValue, &schedule)
+	today := int(time.Now().Weekday())
+
+	for _, day := range schedule {
+		if day.Day == today {
+			return day.Message, nil
+		}
+	}
+
+	return msg, nil
 }
